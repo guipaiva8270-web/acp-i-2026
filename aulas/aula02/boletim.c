7ebb996c99db887a9d124c9e9f127f66bc6d96a0
Guilherme de Paiva <gui.paiva8270@gmail.com>
2026-03-18 21:57:17 -0300

    codigos da aula02

---
+++ aulas/aula02/boletim.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int main() {
+
+char tecla_pressionada;
+int numero_inteiro;
+float numero_com_seis_casas;
+double numero_com_doze_casas;
+
+tecla_pressionada = 'A';
+numero_inteiro = 10;
+numero_com_seis_casas = 0.123456f;
+numero_com_doze_casas= 0.123456;
+
+float nota_a1 = 4.5f;
+float nota_a2 = 8.3f;
+float nota_a3 = 5.7f;
+float media_final =  6.5f;
+
+printf("------------------------\n");
+printf(".      BOLETIM DE NOTAS     .\n");
+printf("------------------------\n");
+printf("Disciplinas      A1    A2     A3   MF\n");
+printf("APC1            %5.1f %5.1f %5.1f %5.1f\n", nota_a1, nota_a2, nota_a3, media_final);
+
+nota_a1 = 4,2f;
+nota-a2 = 3.0f;
+nota_a3 = 1.0f;
+media_final = 4.0f;
+
+printf ("MD     %5.1f %5.1f %5.1f %5.1f\n", nota_a1, nota_a2, nota_a3, media_final);
+
+nota_a1 = 9.0f;
+nota_a2 = 1.0f;
+nota_a3 = 2.0f;
+media_final - 5.0f;
+
+printf ("CALC1 %5.1f %5.1f %5.1f %5.1f\n" , nota_a1, nota_a2, nota_a3, media_final);
+
+    return 0;
+}
+
